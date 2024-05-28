6ff2daeacfb38f65434a91dc7e4d7d057bf83992
Aftab Ahmed <aftab.ahmed@newfold.com>
2024-05-29 01:32:33 +0530

    Local Branch to be installed

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,6 @@ const userRouter = express.Router();
 userRouter.route("/register").post(register);
 userRouter.route("/login").post(login);
 userRouter.route("/id/:userId").get(getUser);
-userRouter.route('/resetPassword').post(resetPassword);
+userRouter.route('/resetPassword').post(resetPassword); 
 
 export default userRouter;
