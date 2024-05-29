6c4087141fb8bd590f6cc802aa7c6129f7a409c5
Aftab Ahmed <aftab.ahmed@newfold.com>
2024-05-29 09:48:48 +0530

    Create local admin api

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getUser, login, register, resetPassword } from '../controllers/userController.js';
+import { createLocalAdmin, getUser, login, register, resetPassword } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
 userRouter.route("/register").post(register);
 userRouter.route("/login").post(login);
 userRouter.route("/id/:userId").get(getUser);
-userRouter.route('/resetPassword').post(resetPassword);
+userRouter.route('/resetPassword').post(resetPassword); 
+userRouter.route("/localAdmin").post(createLocalAdmin);
 
 export default userRouter;
