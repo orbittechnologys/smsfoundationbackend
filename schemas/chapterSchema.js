a49d5debb736c8a1b266de54264cacb646ae47be
[zain baig] <[zain.baig98gmail.com]>
2024-09-30 13:27:01 +0530

    changes

--- schemas/chapterSchema.js
+++ schemas/chapterSchema.js
@@ -1,18 +1,16 @@
 import mongoose from "mongoose";
 
-
 const chapterSchema = mongoose.Schema({
-    chapterUrl:{type:String,required:true,unique:true},
-    audioUrl:{type:String,},
-    videoUrl:{type:String}, 
-    name:{type:String},
-    desc:{type:String},
-    subject:{type:mongoose.Schema.Types.ObjectId, ref:"subject"},
-    totalHours:{type:Number,default:0},
-    test:{type:mongoose.Schema.Types.ObjectId, ref:"test"}
-  });
+  chapterUrl: { type: String, required: true, unique: true },
+  audioUrl: { type: String },
+  videoUrl: [{ type: String }],
+  name: { type: String },
+  desc: { type: String },
+  subject: { type: mongoose.Schema.Types.ObjectId, ref: "subject" },
+  totalHours: { type: Number, default: 0 },
+  test: { type: mongoose.Schema.Types.ObjectId, ref: "test" },
+});
 
+const Chapter = mongoose.model("chapter", chapterSchema);
 
-  const Chapter = mongoose.model("chapter",chapterSchema);
-
-  export default Chapter;
+export default Chapter;
